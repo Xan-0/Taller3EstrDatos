26a7373a0cd2896e8a8f2920cee2b23cbfefccae
Xan-0 <johanalexis650@gmail.com>
2025-12-13 00:08:56 -0300

    include path fix

--- src/NodoBHoja.cpp
+++ src/NodoBHoja.cpp
@@ -1,4 +1,4 @@
-#include "NodoBHoja.h"
+#include "../include/NodoBHoja.h"
 
 NodoBHoja::NodoBHoja(int* claves, int orden, bool es_hoja,
 NodoGrafo** datos, NodoBHoja* siguiente_hoja){
@@ -11,4 +11,5 @@ NodoGrafo** datos, NodoBHoja* siguiente_hoja){
 NodoGrafo** NodoBPlusBase::get_datos(){ return this -> datos; }
 NodoBHoja* NodoBPlusBase::get_siguiente_hoja(){ return this -> siguiente_hoja; }
 void NodoBPlusBase::set_datos(NodoGrafo** datos){ this -> datos = datos; }
+
 void NodoBPlusBase::set_siguiente_hoja(NodoBHoja* siguiente_hoja){ this -> siguiente_hoja = siguiente_hoja; }

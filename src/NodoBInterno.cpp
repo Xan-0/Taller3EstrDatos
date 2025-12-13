d024d8a770f04b1655019fcea5cdfdebe9700f55
Xan-0 <johanalexis650@gmail.com>
2025-12-13 00:09:26 -0300

    include path fix

--- src/NodoBInterno.cpp
+++ src/NodoBInterno.cpp
@@ -1,4 +1,4 @@
-#include "NodoBInterno.h"
+#include "../include/NodoBInterno.h"
 
 NodoBInterno::NodoBInterno(int* claves, int orden, bool es_hoja, NodoBPlusBase** punteros){
     NodoBPlusBase(claves, orden, es_hoja);
@@ -9,4 +9,5 @@ int NodoBInterno::buscar_siguiente(){
     
 }
 NodoBPlusBase** NodoBInterno::get_punteros(){ return this -> punteros;}
+
 void NodoBInterno::set_punteros(NodoBPlusBase** punteros){ this -> punteros = punteros;}

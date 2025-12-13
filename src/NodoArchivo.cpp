d1ca3a86844b0336d4cb7c0c35364d04156e354d
Xan-0 <johanalexis650@gmail.com>
2025-12-13 00:08:21 -0300

    include path fix

--- src/NodoArchivo.cpp
+++ src/NodoArchivo.cpp
@@ -1,4 +1,4 @@
-#include "NodoArchivo.h"
+#include "../include/NodoArchivo.h"
 
 NodoArchivo::NodoArchivo(int id, vector<int> *padres, int size, int type) : NodoGrafo(id, padres){
     this -> size = size;
@@ -9,3 +9,4 @@ int NodoArchivo::get_size(){ return this -> size; }
 int NodoArchivo::get_type(){ return this -> type; }
 void NodoArchivo::set_size(int size){ this -> size = size; }
 void NodoArchivo::set_type(int type){ this -> type = type; }
+

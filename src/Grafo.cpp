60aa5ed7d0e55e7d9db1c39e9ce25122b1689835
Xan-0 <johanalexis650@gmail.com>
2025-12-12 23:44:17 -0300

    Estructura inicial del Grafo (cpp)
    (incompleto)

---
+++ src/Grafo.cpp
@@ -0,0 +1,29 @@
+#include "Grafo.h"
+#include <stack>
+
+Grafo::Grafo(NodoGrafo *raiz, ArbolBPlus *arbol){ this -> raiz = raiz; this -> arbol = arbol; }
+NodoGrafo *Grafo::get_raiz(){ return raiz; }
+ArbolBPlus *Grafo::get_arbolB() { return arbol; }
+void Grafo::set_raiz(NodoGrafo *raiz){ this -> raiz = raiz; }
+void Grafo::set_arbolB(ArbolBPlus *arbol) { this -> arbol = arbol; }
+
+int calcular_espacio_ocupado(int id_directorio){
+    int total;
+    stack<int> s;
+    vector<int> visitados(0,false);
+    
+    NodoGrafo *cursor = raiz;
+    int cursor_id = raiz -> get_id();
+    
+    s.push(cursor_id);
+    
+    while(cursor -> es_directorio()){
+        cursor_id = s.pop();
+        
+        
+        cursor = cursor -> lista_hijos();
+    }
+    
+    
+    return total;
+}

0b27d2e010deef496788b56033bf251aa6a230ef
Xan-0 <johanalexis650@gmail.com>
2025-12-13 00:09:47 -0300

    include path fix

--- src/NodoBPlusBase.cpp
+++ src/NodoBPlusBase.cpp
@@ -1,4 +1,4 @@
-#include "NodoBPlusBase.h"
+#include "../include/NodoBPlusBase.h"
 
 NodoBPlusBase::NodoBPlusBase(int* claves, int orden, bool es_hoja){
     this -> claves = claves;
@@ -11,4 +11,5 @@ int *NodoArchivo::get_claves(){ return this -> claves; }
 bool NodoArchivo::get_es_hoja(){ return this -> es_hoja; }
 void NodoArchivo::set_orden(int orden){ this -> orden = orden; }
 void NodoArchivo::set_claves(int *claves){ this -> claves = claves; }
+
 void NodoArchivo::set_es_hoja(bool es_hoja){ this -> es_hoja = es_hoja; }

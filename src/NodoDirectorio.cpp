49f59359cf556bfccea5b5cbfc97c85a17c99d6d
Xan-0 <johanalexis650@gmail.com>
2025-12-12 23:37:46 -0300

    Refactor NodoDirectorio methods
    Refactor NodoDirectorio constructor.

--- src/NodoDirectorio.cpp
+++ src/NodoDirectorio.cpp
@@ -1,9 +1,15 @@
 #include "NodoDirectorio.h"
 
-NodoDirectorio::NodoDirectorio(int id, vector<int> *padres, vector<int> *hijos){
-    NodoGrafo(id, padres);
+NodoDirectorio::NodoDirectorio(int id, vector<int> *padres, vector<int> *hijos) : NodoGrafo(id, padres){
     this -> hijos = hijos;
 }
 
-int *NodoDirectorio::lista_hijos(){ return this -> hijos; }
+int *NodoDirectorio::lista_hijos(){
+    int n = hijos -> size();
+    int* arr = new int[n];
+    for(int i=0;i<n;i++){
+        arr[i] = (*hijos)[i];
+    }
+    return arr;
+}
 void NodoDirectorio::set_hijos(vector<int> *hijos){ this -> hijos = hijos; }
